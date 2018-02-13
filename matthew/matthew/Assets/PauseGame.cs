41a5afd774ce52c48df2a450eac61b75c0fc58d8
Matthew Foreman <soniceroy@gmail.com>
2018-02-13 14:25:14 -0600

    Shading and onclick actions added to pause menu.

--- matthew/matthew/Assets/PauseGame.cs
+++ matthew/matthew/Assets/PauseGame.cs
@@ -3,27 +3,51 @@ using System.Collections.Generic;
 using UnityEngine;
 //using UnityStandardAssets.Characters.FirstPerson;
 
-public class PauseGame : MonoBehaviour {
-	public Transform Pause;
-	public Transform Player;
-	// Update is called once per frame
-	void Update () {
-			if(Input.GetKeyDown(KeyCode.Escape))
-			{
-					if(Pause.gameObject.activeInHierarchy == false)
-					{
-							Pause.gameObject.SetActive(true);
-							Time.timeScale = 0;
-							Player.GetComponent<CharacterController>().enable = false;
-							Cursor.lockState = CursorLockMode.None;
-					}
-					else
-					{
-							Cursor.lockState = CursorLockMode.Locked;
-							Pause.gameObject.SetActive(false);
-							Time.timeScale = 1;
-							Player.GetComponent<CharacterController>().enable = true;
-					}
-			}
-	}
+public class PauseGame : MonoBehaviour
+{
+		public static bool GameIsPaused = false;
+		public GameObject PauseUI;
+		public GameObject Player;
+		// Update is called once per frame
+		void Update ()
+		{
+				if(Input.GetKeyDown(KeyCode.Escape))
+				{
+						if(GameIsPaused)
+						{
+								Resume();
+						}
+						else
+						{
+								Pause();
+						}
+				}
+		}
+
+		public void Resume()
+		{
+				Cursor.lockState = CursorLockMode.Locked;
+				PauseUI.gameObject.SetActive(false);
+				Time.timeScale = 1;
+				Player.GetComponent<CharacterController>().enable = true;
+				GameIsPaused = false;
+		}
+
+		public void Quit()
+		{
+				Debug.Log("Quitting the game...");
+				Application.Quit();
+		}
+
+
+
+		void Pause()
+		{
+				Cursor.lockState = CursorLockMode.None;
+				PauseUI.gameObject.SetActive(true);
+				Time.timeScale = 0;
+				Player.GetComponent<CharacterController>().enable = false;
+				GameIsPaused = true;
+		}
+
 }
