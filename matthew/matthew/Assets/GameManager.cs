02be875994ab05d6ada336410b9107d1a4787eeb
eddieling <eddielingg@gmail.com>
2018-02-20 14:14:48 -0600

    added gameover menu
    GameOver menu comes on when
    player falls off the plane

---
+++ matthew/matthew/Assets/GameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameManager : MonoBehaviour {
+
+	bool gameHasEnded = false;
+	public GameObject PauseUI;
+	public GameObject Player;
+
+	public void EndGame (){
+		if (gameHasEnded == false && Input.GetKeyDown(KeyCode.A)) {
+			gameHasEnded = true;
+			//Debug.Log ("Game Over");
+		}
+	}
+
+//	void Pause()
+//	{
+//		Cursor.lockState = CursorLockMode.None;
+//		PauseUI.gameObject.SetActive(true);
+//		Time.timeScale = 0;
+//		Player.GetComponent<CharacterController>().enable = false;
+//		GameIsPaused = true;
+//	}
+
+	void Restart(){
+		SceneManager.LoadScene (SceneManager.GetActiveScene ().name);
+
+	}
+
+}
